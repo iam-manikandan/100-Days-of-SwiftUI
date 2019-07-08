8c711fef0cc3bb6cd24bef419329bfa514353eb6
Rudrank <rudrankriyam@gmail.com>
2019-07-08 16:50:43 +0530

    Add AlbumsView and SongsView

---
+++ DAY 002/Project002/Project002/AlbumsView.swift
@@ -0,0 +1,23 @@
+//
+//  AlbumsView.swift
+//  Project002
+//
+//  Created by Rudrank Riyam on 08/07/19.
+//  Copyright © 2019 100DaysOfSwiftUI. All rights reserved.
+//
+
+import SwiftUI
+
+struct AlbumsView : View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#if DEBUG
+struct AlbumsView_Previews : PreviewProvider {
+    static var previews: some View {
+        AlbumsView()
+    }
+}
+#endif
